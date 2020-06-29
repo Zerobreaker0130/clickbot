af5ff554dfabd4aed1f4b6de0be130ea6bbd26ff
Zerobreaker0130 <57627687+Zerobreaker0130@users.noreply.github.com>
2020-06-29 20:56:59 +0800

    Add files via upload

---
+++ bot.sh
@@ -0,0 +1,19 @@
+ #CONTOH:
+  python main.py +639108149348 bch
+  sleep 2
+  
+  python main.py +639108149348 zec
+  sleep 2
+  
+  python main.py +639108149348 ltc
+  sleep 2
+  
+  python main.py +639108149348 doge
+  sleep 2
+  
+  python main.py +639108149348 btc
+  sleep 2
+  
+  echo "Claiming done, this will continue after 5hrs"
+  sleep 5h
+  sh bot.sh
